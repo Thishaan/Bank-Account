1be5deea434be5c9e00ab1f1ab84d868296e90aa
Thishaan <udia@wisc.edu>
2025-09-08 08:09:05 -0400

    added gitignore

--- source/Account.cpp
+++ source/Account.cpp
@@ -2,11 +2,35 @@
 
 
 
-
-class{
-
+// 
 
 
+    Account::Account(int number, const std::string& name, double initialBalance):
 
-    
-}
+    accountNumber(number), holderName(name), balance(initialBalance){};
+
+    void Account::deposit(double amount){
+
+
+        if(balance>=0 ){
+            this->balance += amount;     
+        }
+
+    }
+    bool Account::withdraw(double amount){
+
+    } // returns false if insufficient funds
+    double Account::getBalance() const{
+        return this->balance;
+    }
+    int Account::getAccountNumber() const{
+           return this->accountNumber;
+    }
+    std::string Account::getHolderName() const{
+
+        return this->holderName;
+    }
+
+    void Account::printStatement() const{
+
+    }
